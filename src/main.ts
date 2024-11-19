f957b75b8f9f80555baea0e34afaa766d6c8854e
Rodolfo Patane <rodolfo@patane.dev>
2024-11-19 19:29:12 -0300

    feat: add nestjs ConfigService and Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Golden Raspberry Awards API')
+    .setDescription('API para consulta de vencedores e indicados')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
+  await app.listen(configService.get<number>('PORT', 0));
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
