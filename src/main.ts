ff73bc05a17c4229bad1bb44af06c8b6b54b3de0
Rodolfo Patane <rodolfo@patane.dev>
2024-11-19 19:33:46 -0300

    fix: e2e error

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('', app, document);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(configService.get<number>('PORT', 0));
   console.log(`Application is running on: ${await app.getUrl()}`);
